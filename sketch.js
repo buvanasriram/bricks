2ea7c3d728de7a7823bada8412c89dc0e60a67b0
buvanasriram <buvana.sriram@gmail.com>
2022-01-18 22:22:52 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,127 @@
+var ball, paddle, edges, bricks;
+var score = 0;
+var lives = 3;
+var gamestate = "serve";
+var playerName = ""
+
+
+
+function setup(){
+    createCanvas(500,500);
+    ball = createSprite(200,200,10,10);
+    ball.shapeColor = "red"
+  
+    paddle = createSprite(200, 350, 120, 10);
+    paddle.shapeColor = "blue";
+  
+    edges = createEdgeSprites();
+  
+    bricks = new Group();
+   // text({playerName}, 200,200);
+  
+    createBrickRow(65, "red");
+    createBrickRow(65+29, "orange");
+    createBrickRow(65+29+29, "green");
+    createBrickRow(65+29+29+29, "yellow");
+}
+
+function draw(){
+    background("black");
+  
+  textSize(20);
+  text("Score: "+score,40,25);
+  text("Lives: "+lives, 40, 45);
+  
+  if(gamestate == "serve"){
+    text("Click to serve the ball.", 120,250);
+    ball.velocityX =0;
+    ball.velocityY =0;
+    ball.x = 200;
+    ball.y = 200;
+  }
+  else if(gamestate =="end") {
+    text("Game Over", 150, 250);
+    ball.remove;
+  }
+  else {
+    gameplay();
+  }
+  
+  drawSprites();
+}
+
+function createBrickRow(y, color) {
+    for(c=0; c<6; c++)
+    {
+      var brick = createSprite(65+54*c,y,50, 25);
+      brick.shapeColor = color;
+      bricks.add(brick);
+    }
+}
+
+function mouseClicked()
+{
+  ball.velocityX = 10;
+  ball.velocityY = 6;
+  
+  if(gamestate == "serve"){
+    gamestate = "play";
+    ball.velocityY = -7;
+    ball.velocityX = -7;
+  }
+  
+}
+
+function brickHit(ball, brick) {
+
+ brick.remove();
+ score = score+5;
+ 
+ if (ball.velocityY > -12 && ball.velocityY < 12) {
+   ball.velocityX *= 1.05;
+   ball.velocityY *= 1.05;
+ }
+  
+}
+
+function lifeover(){
+  lives = lives - 1;
+  if(lives>=1) {
+    gamestate = "serve";
+  }
+  else {
+    gamestate = "end";
+  }
+}
+
+function gameplay(){
+  paddle.x = mouseX;
+  
+  if(paddle.x < 60)
+  {
+    paddle.x = 60;
+  }
+    
+  if(paddle.x > 340)
+  {
+    paddle.x = 340;
+  }
+  
+  ball.bounceOff(edges[0]);
+  ball.bounceOff(edges[1]);
+  ball.bounceOff(edges[2]);
+  
+  ball.bounceOff(bricks, brickHit);
+  ball.bounceOff(paddle)
+  
+  if(!bricks[0])
+  {
+    //console.log("Won");
+    ball.velocityX = 0;
+    ball.velocityY = 0;
+    text("Well Done!!",150,200);
+  }
+  if(ball.isTouching(edges[3])) {
+    lifeover();
+  }
+}
